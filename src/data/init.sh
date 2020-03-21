446919dbc432b134de68760cd90b5ef3bb79725b
Yongha Hwang <yongha.hwang@gmail.com>
2020-03-20 22:57:43 -0400

    pytorch w/ updated data

--- src/data/init.sh
+++
@@ -1,5 +0,0 @@
-cd ..
-git clone git@github.com:tensorflow/models.git
-mkdir ../models
-wget http://download.tensorflow.org/models/object_detection/faster_rcnn_resnet101_coco_11_06_2017.tar.gz -P ../models
-gunzip -c ../models/faster_rcnn_resnet101_coco_11_06_2017.tar.gz | tar x -C ../models
